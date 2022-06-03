60d9b30698431af42c474ff07e29f5640222bcbf
Behruz Kuziyev <b.kuziyev22@gmail.com>
2022-06-03 20:03:28 +0500

    kanban route updated

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
@@ -8,16 +7,6 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: () => import('../views/AboutView.vue')
-  },
-  {
-    path: "/kanban",
-    name: "KanbanIndex",
     component: () => import("../views/kanban/index.vue"),
   },
 ]
